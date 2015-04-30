0d097b8aa18a02dfddfb7ea458a3c6f13921b2ba
rreeggkk <rreeggkk1@gmail.com>
2015-04-30 16:03:33 -0400

    1.0.0

---
+++ Thermometer/Source/EmptyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Thermometer
+{
+	public class ModuleThermometer : PartModule
+	{
+		[KSPField(isPersistant = false, guiActive = true, guiName = "Temperature", guiFormat = "F3", guiUnits = "C")]
+		public double temperature = 0;
+
+		public override void OnUpdate() {
+			temperature = this.part.temperature - 273.15;
+		}
+	}
+}
+
